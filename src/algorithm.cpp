57c8998b25ebfc7c5a77aff80f9031756206a139
Yanwen Xu <xuyanwen2012@gmail.com>
2025-02-10 17:56:42 -0800

    save

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -1,13 +1,22 @@
 #include "algorithm.hpp"
 
+#include "shaders/all_shaders.hpp"
+
 namespace vulkan {
 
 Algorithm::Algorithm(VulkanMemoryResource* mr_ptr, const std::string_view shader_name)
     : mr_ptr_(mr_ptr), shader_name_(shader_name) {
-  // load_compiled_shader();
+  load_compiled_shader(shader_name_);
 }
 
-Algorithm::~Algorithm() {
+Algorithm::~Algorithm() {}
+
+// -------------------------------------------------------------------------------------------------
+//
+// -------------------------------------------------------------------------------------------------
+
+void Algorithm::load_compiled_shader(const std::string& shader_name) {
+  // auto shader_pair = shaders::all_shaders.at(shader_name);
 }
 
 }  // namespace vulkan
--- src/shaders/all_shaders.hpp
+++ src/shaders/all_shaders.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <unordered_map>
+#include <utility>
 
 namespace vulkan {
 
@@ -38,51 +39,49 @@ namespace shaders {
 #include "h/tree_move_dups_spv.h"
 #include "h/tree_naive_prefix_sum_spv.h"
 
-static std::unordered_map<std::string, std::pair<const unsigned char*, size_t>> all_shaders = {
-    {"cifar_conv2d", {src_shaders_spv_cifar_conv2d_spv, src_shaders_spv_cifar_conv2d_spv_len}},
-    {"cifar_linear", {src_shaders_spv_cifar_linear_spv, src_shaders_spv_cifar_linear_spv_len}},
-    {"cifar_maxpool2d", {src_shaders_spv_cifar_maxpool2d_spv, src_shaders_spv_cifar_maxpool2d_spv_len}},
-    {"cifar_sparse_conv2d", {src_shaders_spv_cifar_sparse_conv2d_spv, src_shaders_spv_cifar_sparse_conv2d_spv_len}},
-    {"cifar_sparse_linear", {src_shaders_spv_cifar_sparse_linear_spv, src_shaders_spv_cifar_sparse_linear_spv_len}},
-    {"cifar_sparse_maxpool", {src_shaders_spv_cifar_sparse_maxpool_spv, src_shaders_spv_cifar_sparse_maxpool_spv_len}},
-    {"hello_vector_add", {src_shaders_spv_hello_vector_add_spv, src_shaders_spv_hello_vector_add_spv_len}},
-    {"tmp_add_base", {src_shaders_spv_tmp_add_base_spv, src_shaders_spv_tmp_add_base_spv_len}},
-    {"tmp_add_base_v2_16", {src_shaders_spv_tmp_add_base_v2_16_spv, src_shaders_spv_tmp_add_base_v2_16_spv_len}},
-    {"tmp_add_base_v2_32", {src_shaders_spv_tmp_add_base_v2_32_spv, src_shaders_spv_tmp_add_base_v2_32_spv_len}},
-    {"tmp_add_base_v2_64", {src_shaders_spv_tmp_add_base_v2_64_spv, src_shaders_spv_tmp_add_base_v2_64_spv_len}},
-    {"tmp_global_exclusive_scan",
-     {src_shaders_spv_tmp_global_exclusive_scan_spv, src_shaders_spv_tmp_global_exclusive_scan_spv_len}},
-    {"tmp_global_exclusive_scan_v2_16",
-     {src_shaders_spv_tmp_global_exclusive_scan_v2_16_spv, src_shaders_spv_tmp_global_exclusive_scan_v2_16_spv_len}},
-    {"tmp_global_exclusive_scan_v2_32",
-     {src_shaders_spv_tmp_global_exclusive_scan_v2_32_spv, src_shaders_spv_tmp_global_exclusive_scan_v2_32_spv_len}},
-    {"tmp_global_exclusive_scan_v2_64",
-     {src_shaders_spv_tmp_global_exclusive_scan_v2_64_spv, src_shaders_spv_tmp_global_exclusive_scan_v2_64_spv_len}},
-    {"tmp_local_inclusive_scan",
-     {src_shaders_spv_tmp_local_inclusive_scan_spv, src_shaders_spv_tmp_local_inclusive_scan_spv_len}},
-    {"tmp_local_inclusive_scan_v2_16",
-     {src_shaders_spv_tmp_local_inclusive_scan_v2_16_spv, src_shaders_spv_tmp_local_inclusive_scan_v2_16_spv_len}},
-    {"tmp_local_inclusive_scan_v2_32",
-     {src_shaders_spv_tmp_local_inclusive_scan_v2_32_spv, src_shaders_spv_tmp_local_inclusive_scan_v2_32_spv_len}},
-    {"tmp_local_inclusive_scan_v2_64",
-     {src_shaders_spv_tmp_local_inclusive_scan_v2_64_spv, src_shaders_spv_tmp_local_inclusive_scan_v2_64_spv_len}},
-    {"tmp_single_radixsort_warp16",
-     {src_shaders_spv_tmp_single_radixsort_warp16_spv, src_shaders_spv_tmp_single_radixsort_warp16_spv_len}},
-    {"tmp_single_radixsort_warp32",
-     {src_shaders_spv_tmp_single_radixsort_warp32_spv, src_shaders_spv_tmp_single_radixsort_warp32_spv_len}},
-    {"tmp_single_radixsort_warp64",
-     {src_shaders_spv_tmp_single_radixsort_warp64_spv, src_shaders_spv_tmp_single_radixsort_warp64_spv_len}},
-    {"tree_build_octree", {src_shaders_spv_tree_build_octree_spv, src_shaders_spv_tree_build_octree_spv_len}},
-    {"tree_build_radix_tree",
-     {src_shaders_spv_tree_build_radix_tree_spv, src_shaders_spv_tree_build_radix_tree_spv_len}},
-    {"tree_edge_count", {src_shaders_spv_tree_edge_count_spv, src_shaders_spv_tree_edge_count_spv_len}},
-    {"tree_find_dups", {src_shaders_spv_tree_find_dups_spv, src_shaders_spv_tree_find_dups_spv_len}},
-    {"tree_merge_sort", {src_shaders_spv_tree_merge_sort_spv, src_shaders_spv_tree_merge_sort_spv_len}},
-    {"tree_morton", {src_shaders_spv_tree_morton_spv, src_shaders_spv_tree_morton_spv_len}},
-    {"tree_move_dups", {src_shaders_spv_tree_move_dups_spv, src_shaders_spv_tree_move_dups_spv_len}},
-    {"tree_naive_prefix_sum",
-     {src_shaders_spv_tree_naive_prefix_sum_spv, src_shaders_spv_tree_naive_prefix_sum_spv_len}},
+// Helper macro to create shader entry with proper naming convention
+#define SHADER_ENTRY(name)                                                    \
+  {                                                                           \
+    #name, { src_shaders_spv_##name##_spv, src_shaders_spv_##name##_spv_len } \
+  }
+
+// Map of shader names to their binary data and size
+// Key: shader name
+// Value: pair of (shader binary data pointer, shader binary size)
+static const std::unordered_map<std::string, std::pair<const unsigned char*, size_t>> all_shaders = {
+    SHADER_ENTRY(cifar_conv2d),
+    SHADER_ENTRY(cifar_linear),
+    SHADER_ENTRY(cifar_maxpool2d),
+    SHADER_ENTRY(cifar_sparse_conv2d),
+    SHADER_ENTRY(cifar_sparse_linear),
+    SHADER_ENTRY(cifar_sparse_maxpool),
+    SHADER_ENTRY(hello_vector_add),
+    SHADER_ENTRY(tmp_add_base),
+    SHADER_ENTRY(tmp_add_base_v2_16),
+    SHADER_ENTRY(tmp_add_base_v2_32),
+    SHADER_ENTRY(tmp_add_base_v2_64),
+    SHADER_ENTRY(tmp_global_exclusive_scan),
+    SHADER_ENTRY(tmp_global_exclusive_scan_v2_16),
+    SHADER_ENTRY(tmp_global_exclusive_scan_v2_32),
+    SHADER_ENTRY(tmp_global_exclusive_scan_v2_64),
+    SHADER_ENTRY(tmp_local_inclusive_scan),
+    SHADER_ENTRY(tmp_local_inclusive_scan_v2_16),
+    SHADER_ENTRY(tmp_local_inclusive_scan_v2_32),
+    SHADER_ENTRY(tmp_local_inclusive_scan_v2_64),
+    SHADER_ENTRY(tmp_single_radixsort_warp16),
+    SHADER_ENTRY(tmp_single_radixsort_warp32),
+    SHADER_ENTRY(tmp_single_radixsort_warp64),
+    SHADER_ENTRY(tree_build_octree),
+    SHADER_ENTRY(tree_build_radix_tree),
+    SHADER_ENTRY(tree_edge_count),
+    SHADER_ENTRY(tree_find_dups),
+    SHADER_ENTRY(tree_merge_sort),
+    SHADER_ENTRY(tree_morton),
+    SHADER_ENTRY(tree_move_dups),
+    SHADER_ENTRY(tree_naive_prefix_sum),
 };
+
+#undef SHADER_ENTRY
 
 }  // namespace shaders
 
